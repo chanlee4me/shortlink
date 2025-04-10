228adaabc77326bad24c1ec3b72f12f0bd648da3
chanlee4me <cli1004@163.com>
2025-04-10 14:52:53 +0800

    新增用户自定义异常码

---
+++ admin/src/main/java/com/chanlee/shortlink/admin/common/enums/UserErrorCodeEnum.java
@@ -0,0 +1,39 @@
+package com.chanlee.shortlink.admin.common.enums;
+
+import com.chanlee.shortlink.admin.common.convention.errorcode.IErrorCode;
+
+/**
+ * 用户错误码
+ * TODO 后期把它和BaseErrorCode.java进行整合
+ */
+public enum UserErrorCodeEnum implements IErrorCode {
+    USER_NULL("B000200", "用户记录不存在"),
+
+    USER_NAME_EXIST("B000201", "用户名已存在"),
+
+    USER_EXIST("B000202", "用户记录已存在"),
+
+    USER_SAVE_ERROR("B000203","用户记录新增失败"),
+
+    USER_LOGIN_EXIST("B000204", "用户已登录");
+
+    private final String code;
+
+    private final String message;
+
+    UserErrorCodeEnum(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    @Override
+    public String code() {
+        return code;
+    }
+
+    @Override
+    public String message() {
+        return message;
+    }
+}
+
