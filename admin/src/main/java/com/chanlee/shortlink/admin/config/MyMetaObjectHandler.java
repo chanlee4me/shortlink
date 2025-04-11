ae4ac3f54af9f22f9f6b79da5239c698f4dd1b99
chanlee4me <cli1004@163.com>
2025-04-11 18:10:23 +0800

    新增短链接后管系统持久层字段自动填充

---
+++ admin/src/main/java/com/chanlee/shortlink/admin/config/MyMetaObjectHandler.java
@@ -0,0 +1,26 @@
+package com.chanlee.shortlink.admin.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+// java example
+@Slf4j
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "delFlag", Integer.class, 0);
+        this.strictInsertFill(metaObject, "createTime", LocalDateTime.class, LocalDateTime.now());
+        this.strictInsertFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+    }
+}
