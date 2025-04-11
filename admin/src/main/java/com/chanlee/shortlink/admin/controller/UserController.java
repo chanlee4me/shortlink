07401bd3d019327c2b285d380624e5a53e6363a5
chanlee4me <cli1004@163.com>
2025-04-11 16:11:59 +0800

    新增短链接后管系统查询用户信息脱敏功能

--- admin/src/main/java/com/chanlee/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/com/chanlee/shortlink/admin/controller/UserController.java
@@ -1,20 +1,20 @@
 package com.chanlee.shortlink.admin.controller;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.chanlee.shortlink.admin.common.convention.result.Result;
 import com.chanlee.shortlink.admin.common.convention.result.Results;
+import com.chanlee.shortlink.admin.dto.resp.UserActualRespDTO;
 import com.chanlee.shortlink.admin.dto.resp.UserRespDTO;
 import com.chanlee.shortlink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 用户管理控制层
  */
 @RestController
-@RequestMapping("/api/shortlink/v1/user")
 @RequiredArgsConstructor
 public class UserController {
 
@@ -25,8 +25,17 @@ public class UserController {
      * @param username
      * @return
      */
-    @GetMapping("{username}")
+    @GetMapping("/api/shortlink/v1/user/{username}")
     public Result<UserRespDTO> getUserByUsername(@PathVariable String username){
         return Results.success(userService.getUserByUsername(username));
+    }
+    /**
+     * 根据用户名查询无脱敏用户信息
+     * @param username
+     * @return
+     */
+    @GetMapping("/api/shortlink/v1/actual/user/{username}")
+    public Result<UserActualRespDTO> getActualUserByUsername(@PathVariable String username){
+        return Results.success(BeanUtil.copyProperties(userService.getUserByUsername(username), UserActualRespDTO.class));
     }
 }
--- admin/src/main/java/com/chanlee/shortlink/admin/dto/resp/UserActualRespDTO.java
+++ admin/src/main/java/com/chanlee/shortlink/admin/dto/resp/UserActualRespDTO.java
@@ -1,14 +1,12 @@
 package com.chanlee.shortlink.admin.dto.resp;
 
-import com.chanlee.shortlink.admin.common.serialize.PhoneDesensitizationSerializer;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Data;
 
 /**
  * 用户返回参数响应对象
  */
 @Data
-public class UserRespDTO {
+public class UserActualRespDTO {
     /**
      * ID
      */
@@ -27,7 +25,6 @@ public class UserRespDTO {
     /**
      * 手机号
      */
-    @JsonSerialize(using = PhoneDesensitizationSerializer.class)
     private String phone;
 
     /**
