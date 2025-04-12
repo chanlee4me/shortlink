c31e3593318921965522b56a44f9c0f0f34ce9e8
chanlee4me <cli1004@163.com>
2025-04-12 18:34:12 +0800

    新增短链接后管系统缓存标识

--- admin/src/main/java/com/chanlee/shortlink/admin/common/constant/RedisCacheConstant.java
+++ admin/src/main/java/com/chanlee/shortlink/admin/common/constant/RedisCacheConstant.java
@@ -1,5 +1,12 @@
 package com.chanlee.shortlink.admin.common.constant;
 
 public class RedisCacheConstant {
+    /**
+     * 用户注册分布式锁
+     */
     public static final String LOCK_USER_REGISTER_KEY = "shortlink:lock_user_register_key:";
+    /**
+     * 用户 token 黑名单标识（注销时添加）
+     */
+    public static final String USER_LOG_OUT_KEY = "shortlink:user_log_out_key:";
 }
