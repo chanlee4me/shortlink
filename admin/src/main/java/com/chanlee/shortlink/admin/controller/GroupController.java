67f5bedd9d519eb07b7abd7972a3cba45a58b95a
chanlee4me <cli1004@163.com>
2025-04-12 18:29:40 +0800

    短链接后管系统分组模块初始化

---
+++ admin/src/main/java/com/chanlee/shortlink/admin/controller/GroupController.java
@@ -0,0 +1,10 @@
+package com.chanlee.shortlink.admin.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 短链接分组控制层
+ */
+@RestController
+public class GroupController {
+}
