efdb1e54e4a47ef9f43986adfe29f84234e21d18
chanlee4me <cli1004@163.com>
2025-04-13 14:56:35 +0800

    新增后管系统分组功能响应参数对象

---
+++ admin/src/main/java/com/chanlee/shortlink/admin/dto/resp/ShortLinkGroupRespDTO.java
@@ -0,0 +1,29 @@
+package com.chanlee.shortlink.admin.dto.resp;
+
+import lombok.Data;
+
+/**
+ * 分组功能响应参数对象
+ */
+@Data
+public class ShortLinkGroupRespDTO {
+    /**
+     * 分组标识
+     */
+    private String gid;
+
+    /**
+     * 分组名称
+     */
+    private String name;
+
+    /**
+     * 创建分组用户名
+     */
+    private String username;
+
+    /**
+     * 分组排序
+     */
+    private Integer sortOrder;
+}
