2211755bbe10dd4082389ef00899d57ce3a39ab5
chanlee4me <cli1004@163.com>
2025-04-13 14:56:48 +0800

    新增后管系统分组功能请求参数对象

---
+++ admin/src/main/java/com/chanlee/shortlink/admin/dto/req/ShortLinkGroupSaveReqDTO.java
@@ -0,0 +1,15 @@
+package com.chanlee.shortlink.admin.dto.req;
+
+import lombok.Data;
+
+/**
+ * 短链接分组创建参数
+ */
+@Data
+public class ShortLinkGroupSaveReqDTO {
+
+    /**
+     * 分组名
+     */
+    String name;
+}
