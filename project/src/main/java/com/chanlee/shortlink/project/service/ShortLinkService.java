d0cc84372fed7543860e95e1bd5021b7a378fc1e
chanlee4me <cli1004@163.com>
2025-04-15 21:09:44 +0800

    feature：中台系统短链接接口层初始化

---
+++ project/src/main/java/com/chanlee/shortlink/project/service/ShortLinkService.java
@@ -0,0 +1,10 @@
+package com.chanlee.shortlink.project.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.chanlee.shortlink.project.dao.entity.ShortLinkDO;
+
+/**
+ * 短链接接口层
+ */
+public interface ShortLinkService extends IService<ShortLinkDO> {
+}
