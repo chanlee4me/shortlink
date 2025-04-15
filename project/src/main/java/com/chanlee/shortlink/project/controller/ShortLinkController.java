f361157d45327c2eecde2edfae93fdbbac40eb73
chanlee4me <cli1004@163.com>
2025-04-15 21:09:18 +0800

    feature：中台系统短链接控制层初始化

---
+++ project/src/main/java/com/chanlee/shortlink/project/controller/ShortLinkController.java
@@ -0,0 +1,11 @@
+package com.chanlee.shortlink.project.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 短链接控制层
+ */
+@RestController
+public class ShortLinkController {
+
+}
