8f09e1bc46cf34d3da94065290d8b432ea50fd6b
chanlee4me <cli1004@163.com>
2025-04-15 21:09:39 +0800

    feature：中台系统短链接持久层初始化

---
+++ project/src/main/java/com/chanlee/shortlink/project/dao/mapper/ShortLinkMapper.java
@@ -0,0 +1,11 @@
+package com.chanlee.shortlink.project.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.chanlee.shortlink.project.dao.entity.ShortLinkDO;
+
+/**
+ * 短链接持久层
+ */
+public interface ShortLinkMapper extends BaseMapper<ShortLinkDO> {
+
+}
