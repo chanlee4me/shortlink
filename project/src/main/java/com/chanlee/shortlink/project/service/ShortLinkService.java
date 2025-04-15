deaa910a9f8351d730e87463287b8ad4d1b1f088
chanlee4me <cli1004@163.com>
2025-04-15 22:37:44 +0800

    feature：中台系统短链接接口层新增创建短链接接口

--- project/src/main/java/com/chanlee/shortlink/project/service/ShortLinkService.java
+++ project/src/main/java/com/chanlee/shortlink/project/service/ShortLinkService.java
@@ -2,9 +2,18 @@ package com.chanlee.shortlink.project.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.chanlee.shortlink.project.dao.entity.ShortLinkDO;
+import com.chanlee.shortlink.project.dto.req.ShortLinkCreateReqDTO;
+import com.chanlee.shortlink.project.dto.resp.ShortLinkCreateRespDTO;
 
 /**
  * 短链接接口层
  */
 public interface ShortLinkService extends IService<ShortLinkDO> {
+
+    /**
+     * 创建短链接
+     * @param requestParam 短链接创建请求对象
+     */
+    ShortLinkCreateRespDTO createShortLink(ShortLinkCreateReqDTO requestParam);
+
 }
