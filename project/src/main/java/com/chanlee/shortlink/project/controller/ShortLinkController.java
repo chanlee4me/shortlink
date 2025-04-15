59106ef2f7321b7c927f9870134ee6d44b0c6932
chanlee4me <cli1004@163.com>
2025-04-15 22:36:02 +0800

    feature：中台系统短链接控制层新增创建短链接接口

--- project/src/main/java/com/chanlee/shortlink/project/controller/ShortLinkController.java
+++ project/src/main/java/com/chanlee/shortlink/project/controller/ShortLinkController.java
@@ -1,11 +1,27 @@
 package com.chanlee.shortlink.project.controller;
 
+import com.chanlee.shortlink.project.common.convention.result.Result;
+import com.chanlee.shortlink.project.common.convention.result.Results;
+import com.chanlee.shortlink.project.dto.req.ShortLinkCreateReqDTO;
+import com.chanlee.shortlink.project.dto.resp.ShortLinkCreateRespDTO;
+import com.chanlee.shortlink.project.service.ShortLinkService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 短链接控制层
  */
 @RestController
+@RequiredArgsConstructor
 public class ShortLinkController {
+
+    private final ShortLinkService shortLinkService;
+
+    @PostMapping("/api/shortlink/v1/create")
+    public Result<ShortLinkCreateRespDTO> createShortLink(@RequestBody ShortLinkCreateReqDTO requestParam){
+        return Results.success(shortLinkService.createShortLink(requestParam));
+    }
 
 }
