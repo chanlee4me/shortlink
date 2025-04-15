c5ff176ed73247e5665e0ba2ace51fdaba894bc0
chanlee4me <cli1004@163.com>
2025-04-15 21:09:30 +0800

    feature：中台系统短链接持久层实体初始化

---
+++ project/src/main/java/com/chanlee/shortlink/project/dao/entity/ShortLinkDO.java
@@ -0,0 +1,66 @@
+package com.chanlee.shortlink.project.dao.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+/**
+ * 短链接持久层实体
+ */
+@Data
+@TableName("t_link")
+public class ShortLinkDO {
+    @TableId(type = IdType.AUTO)
+    /**
+     * ID
+     */
+    private Long id;
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * 真实姓名
+     */
+    private String realName;
+
+    /**
+     * 手机号
+     */
+    private String phone;
+
+    /**
+     * 邮箱
+     */
+    private String mail;
+
+    /**
+     * 注销时间戳
+     */
+    private Long deletionTime;
+
+    /**
+     * 创建时间
+     */
+    private LocalDateTime createTime;
+
+    /**
+     * 修改时间
+     */
+    private LocalDateTime updateTime;
+
+    /**
+     * 删除标识 0：未删除 1：已删除
+     */
+    private Integer delFlag;
+}
